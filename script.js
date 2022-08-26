e5414b0587888ca7cf91e0a157d91efafe5c7f6a
Masutayunikon <quentinmail974@gmail.com>
2022-08-26 19:07:49 +0400

    👽 transition sympa

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("1").addEventListener("mouseover", function () {
+    document.getElementById("title").innerHTML = "Earth"
+})
+
+document.getElementById("2").addEventListener("mouseover", function () {
+    document.getElementById("title").innerHTML = "Mars"
+})
+
+document.getElementById("3").addEventListener("mouseover", function () {
+    document.getElementById("title").innerHTML = "Moon"
+})
